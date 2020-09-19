35a30d02e84f429b74adf7229c335997f30737ee
Bobby Henderson <bobby.lee.hen@gmail.com>
2020-09-19 15:21:26 -0400

    This is a test

--- chrome_driver.py
+++ chrome_driver.py
@@ -1,3 +1,3 @@
 # Enter location of driver from your machine. 
 # For Chrome go to: https://sites.google.com/a/chromium.org/chromedriver/
-PATH = ''
+PATH = 'https://sites.google.com/a/chromium.org/chromedriver/'
